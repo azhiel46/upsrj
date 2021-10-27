f47d594f809fcd7741b995055a5f602ce743336b
azhiel46 <aziel_balocamp56@hotmail.com>
2021-10-27 00:48:09 -0500

    se agrega proyecto

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+//IMPORT COMPONENTS SECTION
+import ToolBar from "./ToolBar";
+//IMPORT RESOURCE SECTION
+import Horario from "./Resources/Horario.png";
+import LogoBis from "./Resources/LogoBis.png";
+//IMPORT MATERIAL UI SECTION
+import Button from "@mui/material/Button";
+import { makeStyles } from "@mui/styles";
 
-function App() {
+const App = () => {
+  const classes = useStyle();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={classes.root}>
+      <div>
+        <ToolBar />
+      </div>
+
+      <div className={classes.rootHeader}>
+        <Button href="https://upsrj.edu.mx/">
+          <img src={LogoBis} />
+        </Button>
+      </div>
+
+      <div className={classes.horario}>
+        <img src={Horario} />
+      </div>
+
+      <div className={classes.btnClases}>
+        <Button size="large" variant="contained" disableElevation>
+          Clases
+        </Button>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
+
+const useStyle = makeStyles({
+  rootHeader: {
+    display: "flex",
+    justifyContent: "center",
+    paddingTop: "25px",
+  },
+  horario: {
+    display: "flex",
+    justifyContent: "center",
+    paddingTop: "15px",
+  },
+  btnClases: {
+    display: "flex",
+    justifyContent: "center",
+    paddingTop: "20px",
+    
+  },
+});
---
+++ src/ToolBar.js
@@ -0,0 +1,69 @@
+import React from "react";
+//IMPORT COMPONENTS SECTION
+
+//IMPORT MATERIAL UI SECTION
+import InstagramIcon from "@mui/icons-material/Instagram";
+import { makeStyles } from "@mui/styles";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+
+const ToolBar = () => {
+  const classes = useStyle();
+
+  return (
+    <div>
+      <div className={classes.rootHeader}>
+        <div>
+          <Button href="https://www.instagram.com/elpincheaziel/">
+            <InstagramIcon
+              sx={{ fontSize: 55 }}
+              className={classes.InstaIcon}
+            ></InstagramIcon>
+          </Button>
+        </div>
+
+        <div className={classes.btnsHeader}>
+          <Button
+            size="large"
+            variant="text"
+            href="https://upsrj.edu.mx/sistemaintegral/web/alumnos.php"
+          >
+            <Typography variant="h5" color="#ffffff">
+              Site
+            </Typography>
+          </Button>
+          <Button
+            size="large"
+            variant="text"
+            href="https://sites.google.com/upsrj.edu.mx/iswvirtual/c-home/c-3rd-term/c-visual-programming"
+          >
+            <Typography variant="h5" color="#ffffff">
+              SAI
+            </Typography>
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ToolBar;
+
+const useStyle = makeStyles({
+  rootHeader: {
+    display: "flex",
+    paddingTop: "15px",
+    paddingBottom: "15px",
+    backgroundColor: "#457979",
+    justifyContent: "space-around",
+  },
+  InstaIcon: {
+    color: "white",
+  },
+  btn: {
+    color: "red",
+  },
+  btnsHeader:{
+      paddingTop: '5px'
+  }
+});
